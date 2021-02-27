d8d0e588a5b816d93b27f1e548b664c1141d4ea2
jhpark <jhpark.vine@gmail.com>
2021-02-27 14:48:23 +0900

    02 - Decomposing switch Statement

--- ch1/01_starting_program.js
+++ ch1/01_starting_program.js
@@ -22,32 +22,8 @@ function statement(invoice, plays) {
 
         const play = plays[perf.playID];
 
-        let thisAmount = 0;
-
-        switch(play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                
-                if(perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-
-                break;
-            
-            case "comedy":
-                thisAmount = 30000;
-
-                if(perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-
-                thisAmount += 300 * perf.audience;
-
-                break;
-
-            default:
-                throw new Error(`Unknown type: ${play.type}`);
-        }
+        // Change #1 - Extract Function
+        let thisAmount = amountFor(perf, play);
 
         // Add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -64,6 +40,39 @@ function statement(invoice, plays) {
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
 
     result += `You earned ${volumeCredits} credits\n`;
+
+    return result;
+}
+
+// Change #3 - Rename parameter
+function amountFor(aPerformance, play) {
+    // Change #2 - Rename variable
+    let result = 0;
+
+    switch(play.type) {
+        case "tragedy":
+            result = 40000;
+            
+            if(aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
+            }
+
+            break;
+        
+        case "comedy":
+            result = 30000;
+
+            if(aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
+            }
+
+            result += 300 * aPerformance.audience;
+
+            break;
+
+        default:
+            throw new Error(`Unknown type: ${play.type}`);
+    }
 
     return result;
 }
